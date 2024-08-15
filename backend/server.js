695580ae7489e2bd176e524f86ceee06cca5844e
Mayur <mayurpatle108@gmail.com>
2024-08-15 20:45:29 +0530

    mongo db connected

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,9 @@
 const  express =  require('express')  ;  
 const dotenv  = require('dotenv')  ; 
+const connectDB = require('./config/db');
+
+// Connect to the  local mongodb database 
+connectDB()   ;  
 
 // load  environment variables 
 dotenv.config()  ; 
